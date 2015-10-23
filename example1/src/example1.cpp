502d5fd93a4847d3fdffbef8c44c3fbef0357461
Heejung Lee <Heejung.lee@ryerson.ca>
2015-10-23 17:27:04 -0400

    I added a new line to the pragram

---
+++ example1/src/example1.cpp
@@ -0,0 +1,20 @@
+/*
+ * example1.cpp
+ *
+ *  Created on: Oct 23, 2015
+ *      Author: h259lee
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	cout <<"!!!hello world!!!" << endl;
+	cout <<"Eileen" << endl;
+	cout << "a";
+	return 0;
+}
+
+
